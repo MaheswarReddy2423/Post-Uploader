759beb025f90f0f874df9aeb71ddb23117aa40d7
MaheswarReddy2423 <105473885+MaheswarReddy2423@users.noreply.github.com>
2023-04-04 11:58:48 +0530

    commit

---
+++ src/components/HorizentalLine.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const HorizentalLine = () => {
+  return (
+    <View
+      style={{
+        borderBottomWidth: 1,
+        borderBottomColor: 'black',
+      }}
+    />
+  );
+};
+
+export default HorizentalLine;
